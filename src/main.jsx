4338aca448a803ea9e4ea1b4565523b6755a6b9d
João <contato@joaolds.xyz.br>
2026-08-13 15:28:31 -0300

    feat: enable PWA service worker

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <AuthGate />
   </React.StrictMode>,
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js').catch(() => {}))
+}
